85fd9338b9f1c1ada636e3a1c2ebd7118c8ee836
Sergey Ilyevsky <sergeyi@wix.com>
2017-03-20 16:40:54 +0200

    Changed jsCodeLocation to automate loading on devices in debug mode

--- ios/BindingListView/AppDelegate.m
+++ ios/BindingListView/AppDelegate.m
@@ -19,7 +19,7 @@
   NSURL *jsCodeLocation;
 
 #ifdef DEBUG
-  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 #else
   jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
